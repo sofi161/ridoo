6f4c9d7688523912152328234cee2f5d1d39156c
sofi161 <sofipal72@gmail.com>
2025-09-27 15:55:34 +0530

    register route created

--- Backend/app.js
+++ Backend/app.js
@@ -4,15 +4,19 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
 
 connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello ridoo");
 });
 
+app.use("/users", userRoutes);
 // app.listen(3000, (req, res) => {
 //   console.log("Working");
 // });
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,3 +1,26 @@
 const userModel = require("../models/user.model");
+const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
 
-module.exports.registerUser = async (req, res, next) => {};
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  console.log(req.body);
+  const { fullName, email, password } = req.body;
+
+  const hashPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstName: fullName.firstName,
+    lastName: fullName.lastName,
+    email,
+    password: hashPassword,
+  });
+
+  const token = user.generateAuthToken();
+
+  res.status(201).json({ token, user });
+};
